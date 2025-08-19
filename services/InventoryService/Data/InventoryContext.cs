2fffd0002a4dde3749f44c88e08b12f781f88d58
Angélica França <angelicafranca94@hotmail.com>
2025-08-19 20:19:03 -0300

    Conectando com o banco de dados na Azure

---
+++ services/InventoryService/Data/InventoryContext.cs
@@ -0,0 +1,15 @@
+﻿using InventoryService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Data
+{
+    public sealed class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
